745a87d157b9b3b5a0ebea6c45554a2a176a56c1
caesarNetyet <zaevangelion@gmail.com>
2022-07-22 17:51:11 -0500

    Agregamos mas cosas

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <?php include('partials/header.php') ?>
 
 <!-- body -->
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleInterval" class=" carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="./assets/img/BANNERthe-last-of-us-ii-tercer-videojuego-mas-vendido-ano-17-07-2020.webp" class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="..." class="d-block w-100" alt="...">
+    <div class="carousel-item elden" data-bs-interval="2000">
+      <img src="./assets/img/elden-ring-new-header.webp" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="./assets/img/ps-indies-banner-image-03-ps4-en-20jun22.jfif" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -24,6 +24,7 @@
   </button>
 </div>
 <h1 class="text-center">Esto ya va jalando maomeno</h1>
+<p>Como asi pa </p>
 
 <!-- body -->
 
